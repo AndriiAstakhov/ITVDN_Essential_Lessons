3e72293e2557d9f884fe66c9d1b6031cc8e7fe94
Nicoteen88 <astakhov.andrei88@gmail.com>
2016-10-14 16:49:08 +0300

    Add project files.

---
+++ ITVDN_Essential_Lessons/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITVDN_Essential_Lessons
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("New Item");
+        }
+    }
+}
