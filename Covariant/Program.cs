96c6012582bc48ff73f4466372297a6f8a943e57
Nicoteen88 <astakhov.andrei88@gmail.com>
2016-10-15 17:28:09 +0300

    Covariant Arrays

--- Covariant/Program.cs
+++ Covariant/Program.cs
@@ -4,12 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Covariant
+namespace Arrays
 {
+    //Ковариантность массивов в C#.
+
+    public interface IAnimal
+    {
+        void Voice();
+    }
+
+    public class Dog : IAnimal
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Voice");
+        }
+
+        public void Jump()
+        {
+            Console.WriteLine("Jump");
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Dog[] dogs = { new Dog(), new Dog(), new Dog()};
+
+            for (int i = 0; i < dogs.Length; i++)
+            {
+                dogs[i].Voice();
+                dogs[i].Jump();
+            }
+
+            Console.WriteLine(new string('-', 50));
+
+
+            IAnimal[] animal = dogs; //Ковариантность.
+
+            for (int i = 0; i < dogs.Length; i++)
+            {
+                animal[i].Voice();
+                //animal[i].Jump();
+            }
+
+            Console.WriteLine(new string('-', 50));
+
+            dogs = (Dog[])animal; //Не является Контрвариантностью.
+
+            for (int i = 0; i < dogs.Length; i++)
+            {
+                dogs[i].Jump();
+                dogs[i].Voice();
+            }
+
+            //Delay
+            Console.ReadKey();
         }
     }
 }
