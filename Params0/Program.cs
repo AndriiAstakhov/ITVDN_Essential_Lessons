005189b7ef0b1e70531e3b4129d8437613872e92
Nicoteen88 <astakhov.andrei88@gmail.com>
2016-10-15 19:09:06 +0300

    Params0

---
+++ Params0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params0
+{
+    class Program
+    {
+        static void ShowArray(params int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+        }
+
+        static void Main()
+        {
+            ShowArray( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+            //Delay.
+            Console.ReadKey();
+        }
+    }
+}
